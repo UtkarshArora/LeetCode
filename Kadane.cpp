bff9d67d2ad1447e4f0c0dd602623e86d8840fbb
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-05-27 17:24:53 -0400

    Create Kadane.cpp
    Main idea is that if have a negative sum, you don't carry it forward since it won't add to your maxSum, so you set sum = 0, before the next iteration

---
+++ Kadane.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       
+        int n = nums.size();
+        int maxSum = INT_MIN;
+        int sum = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            sum = sum + nums[i];
+            maxSum = max(sum, maxSum);
+            if(sum < 0) sum = 0;
+        }
+        return maxSum;
+    }
+};
