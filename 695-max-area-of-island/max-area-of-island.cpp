a3a993c91d28ef9b83a7441cecb397bf80c5a900
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-06-21 14:28:33 -0500

    Time: 0 ms (100.00%) | Memory: 27.2 MB (97.81%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int findArea(vector<vector<int>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size())
-            return 0;
-
-        if (j < 0 || j >= grid[0].size())
-            return 0;
-
-        if (grid[i][j] != 1)
+        if ((i < 0 || i >= grid.size()) || (j < 0 || j >= grid[0].size()) || grid[i][j] != 1)
             return 0;
 
         if (grid[i][j] == 1) {
-            grid[i][j] = -1;
+            grid[i][j] = 0;
             return 1 + findArea(grid, i + 1, j) + findArea(grid, i, j + 1) +
                    findArea(grid, i - 1, j) + findArea(grid, i, j - 1);
         }
