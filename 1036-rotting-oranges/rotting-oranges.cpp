be353fe1b1a85322f6ff02784c0db872ee994b10
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-06-21 17:17:17 -0500

    Time: 3 ms (30.68%) | Memory: 17.1 MB (34.36%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,10 +3,13 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
 
         int n = grid.size(), m = grid[0].size();
-        int row[] = {-1, 0, 1, 0};
-        int col[] = {0, 1, 0, -1};
+        
+        vector<pair<int,int>>dirs = {{-1,0}, {0,1}, {1,0}, {0,-1}};
+        
         vector<vector<bool>> visited(n, vector<bool>(m, false));
+        
         queue<pair<int, int>> q;
+        
         int empty = 0;
         int rotten = 0;
 
@@ -15,10 +18,8 @@ public:
             for (int j = 0; j < m; j++) {
 
                 if (grid[i][j] == 2) {
-
-                    pair<int, int> p1(i, j);
                     visited[i][j] = true;
-                    q.push(p1);
+                    q.push({i,j});
                 }
                 if(grid[i][j] == 0)
                     empty++;
@@ -32,29 +33,21 @@ public:
 
             while (count > 0) {
 
-                pair<int, int> p = q.front();
-
+                auto[x,y] = q.front();
                 q.pop();
-
-                //visited[p.first][p.second]
                 count--;
 
-                for (int i = 0; i < 4; i++) {
+                for (auto [dx, dy] : dirs) {
 
-                    int nR = p.first + row[i];
-
-                    int nC = p.second + col[i];
+                    int nR = x + dx;
+                    int nC = y + dy;
 
                     if (nR >= 0 && nR < n && nC >= 0 && nC < m &&
                         grid[nR][nC] == 1) {
-                        pair<int, int> p2(nR, nC);
 
                         grid[nR][nC] = 2;
-
                         visited[nR][nC] = true;
-
-                        q.push(p2);
-
+                        q.push({nR, nC});
                         added = true;
                     }
                 }
