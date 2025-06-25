9f10315a66c58dd26bdfd83d4d3d6d070fc56655
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-06-25 13:51:16 -0500

    Time: 7 ms (19.94%) | Memory: 28 MB (5.57%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class hD{
+    public:
+        int height;
+        int dia;
+    hD()
+    {
+        height = 0;
+        dia = 0;
+    }
+    hD(int height, int dia)
+    {
+        this->height = height;
+        this->dia = dia;
+    }
+};
+class Solution {
+public:
+
+    hD hDia(TreeNode* root)
+    {
+        if(root == NULL)
+            return *(new hD(0,0));
+        
+        hD lhD = hDia(root->left);
+        hD rhD = hDia(root->right);
+
+        int height = 1 + max(lhD.height, rhD.height);
+        int diam = max(max(lhD.dia, rhD.dia), (lhD.height + rhD.height));
+
+        hD ans = *(new hD(height, diam));
+        return ans;
+        //return 1 + max(height(root->left), height(root->right));
+    }
+    int maxDiam(TreeNode* root, int& maxDia)
+    {
+        if(root == NULL)
+            return 0;
+        hD lh = hDia(root->left);
+        hD rh = hDia(root->right);
+        int dia = max((lh.height+rh.height), max(lh.dia, rh.dia));
+        maxDia = max(dia, maxDia);
+        return maxDia;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+       
+        int maxDia = 0;
+        return maxDiam(root, maxDia);
+    }
+};
