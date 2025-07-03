f227240d6bf7e1320174307397bf5d25c9e82eb0
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-03 01:54:09 -0500

    Time: 0 ms (100.00%) | Memory: 24.4 MB (66.25%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void preOrder(TreeNode* root, int k, int& count, int& ans)
+    {
+        if(root == NULL)
+            return;
+        
+        preOrder(root->left, k, count, ans);
+        count++;
+        if(count == k)
+            {
+                ans = root->val;
+                return;
+            }
+        preOrder(root->right, k, count, ans);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        
+        int count = 0;
+        int ans = -1;
+        preOrder(root, k, count, ans);
+        return ans;
+    }
+};
