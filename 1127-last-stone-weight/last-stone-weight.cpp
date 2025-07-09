1490a2a3004b3ecb2d67cc548da479f145b30d7a
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-09 13:56:20 -0500

    Time: 0 ms (100.00%) | Memory: 9.9 MB (76.58%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+   
+
+    int lastStoneWeight(vector<int>& stones) {
+
+        if(stones.size() < 2)
+            return stones[0];
+        
+        priority_queue<int>pq(stones.begin(), stones.end());
+
+        while(pq.size() >= 2)
+        {
+            int y = pq.top(); pq.pop();
+            int x = pq.top(); pq.pop();
+
+            if(x!= y)
+            {
+                int diff = y - x;
+                pq.push(diff);
+            }
+        }
+        return (pq.size() == 0)? 0 : pq.top();
+    }
+};
