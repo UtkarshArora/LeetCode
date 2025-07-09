7d7a368c39b7b2d525dd7f9a1eda15720bf773b7
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-09 15:54:28 -0500

    Time: 9 ms (51.13%) | Memory: 26.8 MB (25.27%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,59 +1,46 @@
 class Solution {
 public:
+    vector<vector<bool>> atV, pacV;
+    vector<vector<int>> ans;
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
 
         int n = heights.size(), m = heights[0].size();
-        vector<vector<bool>> atVisited(n, vector<bool>(m, false));
-        vector<vector<bool>> pacVisited(n, vector<bool>(m, false));
-        vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        vector<vector<int>>res;
-        queue<pair<int, int>> atQ, pacQ;
+        atV = pacV = vector<vector<bool>>(n, vector<bool>(m, false));
 
         for (int i = 0; i < n; i++) {
-            pacQ.push({i, 0});
-            atQ.push({i, m - 1});
-            pacVisited[i][0] = true; atVisited[i][m-1] = true;
+            dfs(heights, i, 0, pacV);
+            dfs(heights, i, m - 1, atV);
         }
         for (int i = 0; i < m; i++) {
-            pacQ.push({0, i});
-            atQ.push({n - 1, i});
-            pacVisited[0][i] = true; atVisited[n-1][i] = true;
+            dfs(heights, 0, i, pacV);
+            dfs(heights, n - 1, i, atV);
         }
-        while (!atQ.empty()) {
-            auto [i, j] = atQ.front();
-            atQ.pop();
-            //atVisited[i][j] = true;
-            for (int dir = 0; dir < 4; dir++) {
-                int x = i + dirs[dir][0];
-                int y = j + dirs[dir][1];
-                if (x < 0 || x >= n || y < 0 || y >= m || atVisited[x][y] ||
-                    heights[x][y] < heights[i][j])
-                    continue;
-                atVisited[x][y] = true;
-                atQ.push({x,y});
-            }
-        }
-        while (!pacQ.empty()) {
-            auto [i, j] = pacQ.front();
-            pacQ.pop();
-            for (int dir = 0; dir < 4; dir++) {
-                int x = i + dirs[dir][0];
-                int y = j + dirs[dir][1];
-                if (x < 0 || x >= n || y < 0 || y >= m || pacVisited[x][y] ||
-                    heights[x][y] < heights[i][j])
-                    continue;
-                pacVisited[x][y] = true;
-                pacQ.push({x,y});
-            }
-        }
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < m ; j++)
-            {
-                if(atVisited[i][j] && pacVisited[i][j])
-                    res.push_back({i,j});
-            }
-        }
-        return res;
+        return ans;
     }
+
+    void dfs(vector<vector<int>>& heights, int i, int j,
+             vector<vector<bool>>& visited) {
+
+                int n = heights.size(), m = heights[0].size();
+                if(i < 0 || i >=n || j < 0 || j >=m || visited[i][j])
+                    return;
+
+                visited[i][j] = true;
+
+                if(atV[i][j] && pacV[i][j])
+                    ans.push_back({i,j});
+                
+                if( i+1 < n && heights[i+1][j] >= heights[i][j])
+                    dfs(heights, i+1, j, visited);
+
+                if( i-1 >= 0 && heights[i-1][j] >= heights[i][j])
+                    dfs(heights, i-1, j, visited);
+                
+                if( j+1 < m && heights[i][j+1] >= heights[i][j])
+                    dfs(heights, i, j+1, visited);
+
+                if( j-1 >= 0 && heights[i][j-1] >= heights[i][j])
+                    dfs(heights, i, j-1, visited);
+             }
 };
