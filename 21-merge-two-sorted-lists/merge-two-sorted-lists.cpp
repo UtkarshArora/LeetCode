f1ea278710ca12c5e4c21f7696767d63dc44b4d4
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-12 16:22:37 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (29.72%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -12,62 +12,29 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-        ListNode* newhead = NULL, *tail = NULL;
+        ListNode* dummy, *temp;
+        dummy = new ListNode();
+        temp = dummy;
 
         while(list1!=NULL && list2!=NULL)
         {
             if(list1->val < list2->val)
             {
-                if(newhead == NULL)
-                    {
-                        newhead = list1;
-                        tail = list1;
-                    }
-                else
-                    {
-                        tail->next = list1;
-                        tail = list1;
-                    }
+                temp->next = list1;
                 list1 = list1->next;
             }
             else
             {
-                if(newhead == NULL)
-                    {
-                        newhead = list2;
-                        tail = list2;
-                    }
-                else{
-                        tail->next = list2;
-                        tail = list2;
-                    }
+                temp->next = list2;
                 list2 = list2->next;
             }
+            temp = temp->next;
         }
-        while(list1)
-        {
-            if(newhead == NULL)
-            {
-                newhead = list1;
-                tail = list1;
-                return newhead;
-            }
-            tail->next = list1;
-            tail = list1;
-            list1 = list1->next;
-        }
-        while(list2)
-        {
-            if(newhead == NULL)
-            {
-                newhead = list2;
-                tail = list2;
-                return newhead;
-            }
-            tail->next = list2;
-            tail = list2;
-            list2 = list2->next;
-        }
-        return newhead;
+        if(list1)
+            temp->next = list1;
+        if(list2)
+            temp->next = list2;
+        
+        return dummy->next;
     }
 };
