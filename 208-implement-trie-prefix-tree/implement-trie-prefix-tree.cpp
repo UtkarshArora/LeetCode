8c950616ccb6ba2d2c122d2dd0652b95d3c01746
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-28 21:21:44 -0500

    Time: 71 ms (9.20%) | Memory: 67.3 MB (8.87%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,57 @@
+class Trie {
+
+private:
+
+    bool compareStrings(string s1, string prefix)
+    {
+        if(s1.size() < prefix.size())
+            return false;
+        
+        int index = 0;
+        while(index < prefix.size())
+        {
+            if(s1[index] == prefix[index])
+                index++;
+            else
+                return false;
+        }
+        return (index == prefix.size());
+    }
+public:
+
+    vector<vector<string>>prefixes;
+    unordered_set<string>words;
+    Trie() {
+        prefixes.resize(26);
+    }
+    
+    void insert(string word) {
+        int index = word[0] - 'a';
+        prefixes[index].push_back(word);
+        words.insert(word);
+    }
+    
+    bool search(string word) {
+        return words.count(word);
+    }
+    
+    bool startsWith(string prefix) {
+        int index = prefix[0] - 'a';
+        vector<string>v1 = prefixes[index];
+        for(string s : v1)
+        {
+            bool ans = compareStrings(s,prefix);
+            if(ans)
+                return ans;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
