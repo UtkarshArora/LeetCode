7b0d604cb270095133dd2855c55b94f8e63176a5
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-28 23:10:59 -0500

    Time: 28 ms (29.97%) | Memory: 53.9 MB (12.41%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,55 +1,81 @@
-class Trie {
-private:
-    bool compareStrings(string s1, string prefix)
-    {
-        if(s1.size() < prefix.size())
-            return false;
-        
-        int index = 0;
-        while(index < prefix.size())
-        {
-            if(s1[index] == prefix[index])
-                index++;
-            else
-                return false;
-        }
-        return (index == prefix.size());
-    }
+class TrieNode{
 
+    public:
+        vector<TrieNode*> next;
+        bool is_word;
+        TrieNode()
+        {
+            next.assign(26, NULL);
+            this->is_word = false;
+        }
+};
+
+class Trie {
 public:
-    vector<vector<string>>prefixes;
-    //unordered_set<string>words;
+    vector<TrieNode*>words;
     Trie() {
-        prefixes.resize(26);
+        words.assign(26, NULL);
     }
     
     void insert(string word) {
+
         int index = word[0] - 'a';
-        prefixes[index].push_back(word);
-        // words.insert(word);
+        TrieNode* node = NULL;
+        if(words[index] == NULL)
+        {
+            node = new TrieNode();
+            words[index] = node;
+        }
+        else
+            node = words[index];
+        
+        for(int i = 1 ; i < word.size() ; i++)
+        {
+            int index = word[i] - 'a';
+
+            if(node->next[index] == NULL){
+                node->next[index] = new TrieNode();
+            }
+            node = node->next[index];
+        }
+        node->is_word = true;
     }
     
     bool search(string word) {
         
         int index = word[0] - 'a';
-        for(string& s1 : prefixes[index])
+
+        if(words[index] == NULL)
+            return false;
+        
+        TrieNode* node = words[index];
+        
+        for(int i = 1 ; i < word.size() ; i++)
         {
-            if(s1 == word)
-                return true;
+            int index = word[i] - 'a';
+            if(node->next[index] == NULL)
+                return false;
+            else
+                node = node->next[index];
         }
-        return false;
+        return node->is_word;
     }
     
     bool startsWith(string prefix) {
+        
         int index = prefix[0] - 'a';
-        vector<string>v1 = prefixes[index];
-        for(string& s : v1)
+        if(words[index] == NULL)
+            return false;
+        TrieNode* node = words[index];
+        
+        for(int i = 1; i < prefix.size() ; i++)
         {
-            bool ans = compareStrings(s,prefix);
-            if(ans)
-                return ans;
+            int index = prefix[i] - 'a';
+            node = node->next[index];
+            if(node == NULL)
+                return false;
         }
-        return false;
+        return true;
     }
 };
 
