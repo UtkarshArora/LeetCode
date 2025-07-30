88d930d9bd904f4bf02400f7dd8ed570fba9c796
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-29 21:20:14 -0500

    Time: 4 ms (78.66%) | Memory: 13.1 MB (65.49%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -6,20 +6,16 @@ public:
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     };
         int count = 0;
-        int maxCount = 0;
         for (int i = 0; i < k; i++) {
             if (isVowel(s[i])) {
                 count++;
-                maxCount = max(count, maxCount);
             }
         }
+        int maxCount = count;
         int start = 0;
         int end = k;
         while (end < s.size()) {
-            if (isVowel(s[start++]))
-                count--;
-            if (isVowel(s[end++]))
-                count++;
+            count += isVowel(s[end++]) - isVowel(s[start++]);
             maxCount = max(count, maxCount);
         }
         return maxCount;
