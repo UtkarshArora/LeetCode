4705e9ed82beefdb1b48d8d69296744451e06ee1
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-07-30 23:01:32 -0500

    Time: 0 ms (100.00%) | Memory: 10.5 MB (80.66%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-
-    int countOnes(int num)
-    {
-        int digit = 1;
-        int count = 0;
-        while(num)
-        {
-            count = count + (digit & num);
-            num = num >> 1;
-        }
-        return count;
-    }
     vector<int> countBits(int n) {
+
         vector<int>ans(n+1);
-        for(int i = 0 ; i <=n ; i++)
+        ans[0] = 0;
+        for(int i = 1 ; i <=n ; i++)
         {
-            ans[i] = countOnes(i);
+            ans[i] = ans[i/2] + i%2;
         }
         return ans;
     }
