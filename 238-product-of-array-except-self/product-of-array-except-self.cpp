8dfdc85dfdaf4cea9ff610e11845fb025f2ed162
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-08-05 13:01:49 -0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (57.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,7 +4,7 @@ public:
         
         int n = nums.size();
         vector<int>suffix(n,1);
-        vector<int>ans(n, 1);
+        //vector<int>ans(n, 1);
         for(int i = n-2; i >=0 ; i--)
         {
             suffix[i] = suffix[i+1] * nums[i+1];
