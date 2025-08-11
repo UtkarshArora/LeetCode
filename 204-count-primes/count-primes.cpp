516200988a78f0b53e72f2590fdfe6f1272cce25
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-08-11 16:18:34 -0500

    Time: 399 ms (8.86%) | Memory: 132.5 MB (23.74%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        int maxCount = floor(sqrt(n));
+        vector<int>primeNums(n+1,1);
+        int count = 0;
+        for(int i = 2 ; i <=maxCount ; i++)
+        {   
+            for(int j = 2 ; (i*j)<=n ; j++)
+            {
+                int num = i*j;
+                primeNums[num] = 0;
+            }
+        }
+        for(int i = 2 ; i < n ; i++)
+        {
+            count += primeNums[i];
+        }
+        return count;
+    }
+};
