9a8145e7b9cb9350a1815dba45b5cf31faea3486
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-08-20 14:10:56 -0500

    Time: 111 ms (6.52%) | Memory: 47.7 MB (41.53%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        int currTime = 0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+
+        vector<vector<int>>adjM(n+1, vector<int>(n+1, -1));
+
+        for(int i = 0 ; i < times.size() ; i++)
+        {
+            vector<int>v1 = times[i];
+            int x = v1[0], y = v1[1], time = v1[2];
+            adjM[x][y] = time;
+        }
+        pq.push({currTime,k});
+        unordered_set<int>set1;
+        while(!pq.empty())
+        {
+            auto [time,curr] = pq.top();
+            pq.pop();
+
+            if(set1.count(curr))
+                continue;
+            set1.insert(curr);
+            currTime = time;
+
+            cout<<"currTime: "<<currTime<<", curr:"<<curr<<endl;
+            for(int i = 1; i <= n ; i++)
+            {
+                int val = adjM[curr][i];
+                if(val!= -1)
+                {
+                    int time = currTime + val;
+                    pq.push({time, i});
+                }
+            }
+        }
+        return set1.size() == n ? currTime : -1;
+    }
+};
