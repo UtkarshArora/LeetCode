65aefd932247989d0a35763209f194c834bbefc0
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-08-20 15:53:00 -0500

    Time: 3 ms (28.21%) | Memory: 29 MB (71.35%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -6,37 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-
-    bool checkIdentical(TreeNode* root, TreeNode* subRoot)
-    {
+    bool checkIdentical(TreeNode* root, TreeNode* subRoot) {
         if (!root && !subRoot)
             return true;
 
         if (!root || !subRoot)
             return false;
 
-        return root->val == subRoot->val && checkIdentical(root->left, subRoot->left) && checkIdentical(root->right, subRoot->right);
+        return root->val == subRoot->val &&
+               checkIdentical(root->left, subRoot->left) &&
+               checkIdentical(root->right, subRoot->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
 
-        if (!root && !subRoot)
-            return true;
-
-        if (!root || !subRoot)
+        if (root == NULL)
             return false;
 
-        bool ans = false;
-        ans = ans || isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-  
-        if(root->val == subRoot->val)
-        {
-            ans = ans || checkIdentical(root, subRoot);
-        }
+        bool ans =
+            isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+
+        ans = ans || checkIdentical(root, subRoot);
         return ans;
     }
 };
