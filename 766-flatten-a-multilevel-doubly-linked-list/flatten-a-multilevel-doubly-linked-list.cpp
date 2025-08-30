2583f2f5170106b5bc873255fa612b372a8515d1
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-08-30 16:33:45 -0400

    Time: 0 ms (100.00%) | Memory: 11.7 MB (72.49%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    stack<Node*> levels;
+    Node* flatten(Node* head) {
+
+        Node* newhead = head;
+        Node* curr = head;
+        Node* tail = head;
+
+        while(curr) {
+            cout<<curr->val<<endl;
+            if (curr->child!=NULL){
+                if(curr->next!=NULL)
+                    levels.push(curr->next);
+                curr = curr->child;
+                curr->prev = tail;
+                tail->next = curr;
+                tail->child = NULL;
+                tail = tail->next;
+            } else if (curr->next) {
+                curr = curr->next;
+                curr->prev = tail;
+                tail->next = curr;
+                tail->child = NULL;
+                tail = tail->next;
+            } else {
+                //cout<<"tail:"<<tail->val<<endl;
+                if(levels.empty())
+                    return newhead;
+                curr = levels.top();
+                levels.pop();
+                tail->next = curr;
+                curr->prev = tail;
+                tail = curr;
+                curr->child = NULL;
+            }
+        }
+        return newhead;
+    }
+};
