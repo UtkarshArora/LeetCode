8ec60b88b4af768386987a08553c12e35d2ccc51
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2025-11-04 01:01:26 -0500

    Time: 3 ms (49.58%) | Memory: 17.6 MB (99.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,33 +1,36 @@
-// struct compare{
-//     bool operator()(pair<int,int>&p1, pair<int,int>&p2)
-//     {
-//         return p1.first < p2.first;
-//     }
-// };
-
 class Solution {
 public:
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
-        unordered_map<int,int>map1;
-        for(int num : nums)
+        
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        priority_queue<pair<int,int>>pq;
+        int count = 0;
+        for(int i = 0 ; i < n; i++)
         {
-            map1[num]++;
+            if(i == 0)
+            {
+                count = 1;
+            }
+            else if(nums[i]!=nums[i-1])
+            {
+                pair<int,int>p1 = {count, nums[i-1]};
+                pq.push(p1);
+                count = 1;
+            }
+            else
+            {
+                count++;
+            }
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>>pq;
-        for(int num : nums)
-        {
-            if(map1.find(num)!=map1.end())
-                pq.push({map1[num], num});
-            map1.erase(num);
-        }
+        pq.push({count,nums[n-1]});
         vector<int>ans;
         while(k)
         {
-            pair<int,int>p1 = pq.top();
+            pair<int,int>p = pq.top();
+            int num = p.second;
+            ans.push_back(num);
             pq.pop();
-            ans.push_back(p1.second);
             k--;
         }
         return ans;
