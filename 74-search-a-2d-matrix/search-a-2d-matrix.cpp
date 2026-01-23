cd90abd784d32bb83042d48bb4ab2b34174da9cf
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-01-23 12:14:09 -0500

    Time: 0 ms (100.00%) | Memory: 13.1 MB (99.86%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int start = 0, end = n-1;
-        int mid = -1;
-        int ans = mid;
+        
+        int m = matrix.size(), n = matrix[0].size();
+        int start = 0, end = m-1, mid = -1;
+        int row = -1;
         while(start <= end)
         {
             mid = start + (end-start)/2;
-            cout<<start<<endl;
-            cout<<end<<endl;
-            if(target == matrix[mid][0])
-                return true;
-            
-            if(target > matrix[mid][0])
+            if(matrix[mid][0] <= target && matrix[mid][n-1] >= target)
             {
-                ans = mid;
+                row = mid;
+                break;
+            }
+            else if(matrix[mid][n-1] < target)
+            {
                 start = mid+1;
             }
-            else if(target < matrix[mid][0])
-            {
-                end = mid - 1;
+            else if(matrix[mid][0] > target){
+                end = mid-1;
             }
         }
-        if(ans == -1)
-            return false;
-        start = 0;
-        end = m-1;
+        row = mid;
+        start = 0, end = n-1;
         while(start <= end)
         {
-            int mid = start + (end - start)/2;
-            if(target == matrix[ans][mid])
+            mid = start + (end - start)/2;
+            if(matrix[row][mid] == target)
+            {
                 return true;
-            else if(target > matrix[ans][mid])
-                start = mid+1;
-            else
+            }
+            else if(matrix[row][mid] > target)
+            {
                 end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
         }
-        return false; 
+        return false;
     }
 };
