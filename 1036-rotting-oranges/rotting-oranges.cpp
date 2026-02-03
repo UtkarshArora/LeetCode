4d6caa32c32f1ebde9520018e2b544ee43e91f9b
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-02-03 12:41:46 -0500

    Time: 4 ms (16.42%) | Memory: 16.7 MB (87.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,61 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        
+
         int n = grid.size();
         int m = grid[0].size();
-        
-        vector<vector<int>>dirs = {{-1,0},{1,0},{0,1},{0,-1}};
-        queue<pair<int,int>>q;
+        int countFreshOranges = 0;
+        vector<pair<int,int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        queue<pair<int, int>> q;
         int countZeros = 0;
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < m ; j++)
-            {
-                if(grid[i][j] == 2){
-                    q.push(make_pair(i,j));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push(make_pair(i, j));
                 }
-                if(grid[i][j] == 0){
-                    countZeros++;
+                if (grid[i][j] == 1) {
+                    countFreshOranges++;
                 }
-            }   
+            }
         }
-        if(n == 0 || countZeros == m*n)
+        if (n == 0 || countFreshOranges == 0)
             return 0;
 
-        if(q.empty())
+        if (q.empty())
             return -1;
 
-        int count = 0;
         int minutes = -1;
 
-        while(!q.empty())
-        {
+        while (!q.empty()) {
+
             int q_size = q.size();
-            count+=q_size;
-            for(int i = 0 ; i < q_size ; i++)
-            {
-                pair<int,int>p = q.front();
-                int x = p.first, y = p.second;
+
+            for (int i = 0; i < q_size; i++) {
+                auto [x, y] = q.front();
                 q.pop();
-                for(int i = 0 ; i < 4 ; i++)
-                {
-                    int x_new = x + dirs[i][0];
-                    int y_new = y + dirs[i][1];
-                    if(x_new < 0 || x_new == n)
-                        continue;
-                    if(y_new < 0 || y_new == m)
-                        continue;
-                    if(grid[x_new][y_new] == 1){
+                for (auto [dx, dy] : dirs) {
+                    int x_new = x + dx;
+                    int y_new = y + dy;
+                    if (x_new >= 0 && x_new < n && y_new >= 0 && y_new < m &&
+                        grid[x_new][y_new] == 1) {
                         q.push(make_pair(x_new, y_new));
                         grid[x_new][y_new] = 2;
+                        countFreshOranges--;
                     }
                 }
             }
             minutes++;
         }
-        if((countZeros + count) == m*n)
-        {
+        if (countFreshOranges == 0) {
             return minutes;
         }
         return -1;
