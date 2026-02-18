fbdb162db7a5ec63f782a96ce620d0764b46f949
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-02-18 18:23:21 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (29.78%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        
         int n = cost.size();
         vector<int>dp(n,0);
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return cost[0];
         dp[0] = cost[0];
         dp[1] = cost[1];
         for(int i = 2 ; i < n ; i++)
