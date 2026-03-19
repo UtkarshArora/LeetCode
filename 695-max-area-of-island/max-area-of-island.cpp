25a3829ea2814f1f62d8065ea7cbc3b1dc50cb95
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-03-18 20:12:44 -0400

    Time: 24 ms (6.05%) | Memory: 27.3 MB (96.97%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
+    int DFS(vector<vector<int>>& grid, int x, int y) {
+
+    if (x < 0 || y < 0 || x == grid.size() || y == grid[0].size() ||
+            grid[x][y] == 0){
+            return 0;
+        }
+        grid[x][y] = 0;
+       return 1 + DFS(grid, x - 1, y) + DFS(grid, x + 1, y) + DFS(grid, x, y - 1) + DFS(grid, x, y + 1);
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
-        vector<vector<int>>dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-        int area = 0, maxArea = 0;
-        queue<pair<int,int>>q;
+
+        int res = 0;
         int n = grid.size(), m = grid[0].size();
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < m ; j++)
-            {
-                if(grid[i][j] == 1)
-                {
-                    q.push({i,j});
-                    int area = 0;
-                    while(!q.empty())
-                    {
-                        pair<int,int>p1 = q.front();
-                        q.pop();
-                        int x = p1.first, y = p1.second;
-                        grid[x][y] = 0;
-                        area++;
-                        for(int i = 0 ; i < 4 ; i++)
-                        {
-                            int x_new = x + dirs[i][0];
-                            int y_new = y + dirs[i][1];
-                            if(x_new < 0 || x_new >= n)
-                                continue;
-                            if(y_new < 0 || y_new >= m)
-                                continue;
-                            if(grid[x_new][y_new] == 1)
-                            {
-                                q.push({x_new, y_new});
-                                grid[x_new][y_new] = 0;
-                            }
-                        }
-                    }
-                    maxArea = max(area, maxArea);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    int ans = DFS(grid, i, j);
+                    cout<<"i:"<<i<<",j:"<<j<<",ans:"<<ans<<endl;
+                    res = max(res, ans);
                 }
             }
         }
-        return maxArea;
+        return res;
     }
 };
