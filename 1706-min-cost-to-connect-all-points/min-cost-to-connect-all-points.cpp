d47ce911e7bd9b643184c856da3cf234b809f17f
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-03-25 15:50:34 -0400

    Time: 25 ms (86.77%) | Memory: 18.1 MB (80.94%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-
+        
         int n = points.size();
-        vector<int>minDist(n, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+
+        pq.push({0,0});
         vector<bool>inMST(n, false);
-        minDist[0] = 0;
-        int edges = 0;
+        vector<int>minDist(n, INT_MAX);
         int cost = 0;
+        int edges = 0;
         while(edges < n)
         {
-            int index = -1;
-            int dist = INT_MAX;
-            for(int i = 0 ; i < n ; i++)
-            {
-                if(!inMST[i] && dist > minDist[i])
-                {
-                    dist = minDist[i];
-                    index = i;
-                }
+            pair<int,int>p = pq.top();
+            pq.pop();
+            int index = p.second;
+            if(inMST[index]){
+                continue;
             }
+            cost = cost + p.first;
             inMST[index] = true;
-            cost = cost + dist;
             edges++;
             for(int i = 0 ; i < n ; i++)
             {
-                if(inMST[i]){
-                    continue;
+                if(!inMST[i])
+                {
+                    int dist = abs(points[i][0] - points[index][0]) + abs(points[i][1] - points[index][1]);
+                    if(dist < minDist[i]){
+                        minDist[i] = dist;
+                        pq.push({dist, i});
+                    }
                 }
-                int distance = abs(points[i][0] - points[index][0]) + abs(points[i][1] - points[index][1]);
-                minDist[i] = min(distance, minDist[i]);
             }
         }
         return cost;
