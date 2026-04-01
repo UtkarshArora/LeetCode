8b4a3b3771f24fc7944f69b9177f6cbc40191ac0
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-04-01 11:26:31 -0400

    Time: 5 ms (55.43%) | Memory: 20.1 MB (34.33%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        queue<int>q;
-        vector<int>numDep(numCourses);
 
-        vector<vector<int>>adjM(numCourses);
-        for(auto v1 : prerequisites)
+        vector<int>numDep(numCourses, 0);
+        vector<vector<int>>adjList(numCourses);
+        int count = 0;
+        for(auto v : prerequisites)
         {
-            int second = v1[1];
-            int first = v1[0];
-            adjM[second].push_back(first);
-            numDep[first]++;
+            //cout<<v[0] << v[1]<<endl;
+            numDep[v[0]]++;
+            adjList[v[1]].push_back(v[0]);
         }
-        for(int i = 0; i < numCourses; i++)
+        queue<int>q;
+        for(int i = 0 ; i < numCourses ; i++)
         {
-            if(numDep[i] == 0)
+            if(numDep[i] == 0){
                 q.push(i);
+                // cout<<i<<endl;
+            }
         }
-        int count = q.size();
         while(!q.empty())
         {
-            int num = q.front();
+            count++;
+            int node = q.front();
             q.pop();
-            for(int n : adjM[num])
+            //cout<<node<<" "<<count<<" "<<adjList[node].size()<<endl;
+            for(int v1 : adjList[node])
             {
-                numDep[n]--;
-                if(numDep[n] == 0){
-                    q.push(n);
-                    count++;
-                }
+                numDep[v1]--;
+                if(numDep[v1] == 0)
+                    q.push(v1);
             }
         }
         return count == numCourses;
