119bb91f3cafef5a8db144384ebe2dc1024f3afc
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-04-26 12:28:38 -0400

    Time: 5 ms (86.24%) | Memory: 47.1 MB (30.55%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,39 +1,31 @@
 class MinStack {
 
-    TreeMap<Integer, List<Integer>>mp = new TreeMap<>();
-    Stack<Integer>st = new Stack<>();
-    
+    Stack<int[]>st;
     public MinStack() {
-        
+        st = new Stack<>();
     }
     
     public void push(int val) {
-        st.push(val);
-        List<Integer>list1 = mp.getOrDefault(val, new ArrayList<>());
-        list1.add(val);
-        mp.put(val, list1);
+        int[] top = st.isEmpty()? new int[]{val, val} : st.peek();
+        int min_val = top[1];
+        if(val < min_val)
+        {
+            min_val = val;
+        }
+        st.push(new int[]{val, min_val});
     }
     
     public void pop() {
-
-        if(st.isEmpty())
-            return;
-        int val = st.peek();
-        List<Integer> list1 = mp.get(val);
-        list1.remove(list1.size()-1);
-        if(list1.size()!= 0)
-            mp.put(val, list1);
-        else
-            mp.remove(val);
         st.pop();
+        return;
     }
     
     public int top() {
-        return st.peek();
+        return st.peek()[0];
     }
     
     public int getMin() {
-        return mp.firstKey();
+        return st.peek()[1];
     }
 }
 
