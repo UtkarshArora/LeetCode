14c02a2faea2d01370bbb628aed645ab0d881512
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-05-18 19:17:44 -0400

    Time: 0 ms (100.00%) | Memory: 40.3 MB (58.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -18,19 +18,35 @@ public:
         //     ans[i] = prod;
         // }
 
-        // number 2 solution: have prod of the numbers to the right
-        vector<int>prodNums(n, 0);
+        // number 2 solution: have prod of the numbers to the right, basically we store the product so that we don't have to use 2 loops
+        // vector<int>prodNums(n, 0);
+        // int prod = 1;
+        // for(int i = n - 1 ; i >=0 ; i--)
+        // {
+        //     prodNums[i] = prod;
+        //     prod*= nums[i];
+        // }
+        // prod = 1;
+        // for(int i = 0 ; i < n ; i++)
+        // {
+        //     ans[i] = prod * prodNums[i];
+        //     prod*= nums[i];
+        // }
+
+        // number 3 solution: traverse from left, update in the final array
+        // traverse from right, update in the final array
+
         int prod = 1;
-        for(int i = n - 1 ; i >=0 ; i--)
-        {
-            prodNums[i] = prod;
-            prod*= nums[i];
-        }
-        prod = 1;
         for(int i = 0 ; i < n ; i++)
         {
-            ans[i] = prod * prodNums[i];
-            prod*= nums[i];
+            ans[i] = prod;
+            prod*=nums[i];
+        }
+        prod = 1;
+        for(int i = n-1 ; i >=0 ; i--)
+        {
+            ans[i] = ans[i] * prod;
+            prod = prod * nums[i];
         }
         return ans;
     }
