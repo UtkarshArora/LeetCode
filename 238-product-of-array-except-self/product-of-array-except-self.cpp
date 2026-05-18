087825e309c2fb51aba8fbb5afa2a1615c63978a
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-05-18 18:59:35 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (38.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int curr = 1;
+        
+        // number 1 solution: double loop
         int n = nums.size();
-        vector<int>output(n,1);
 
+        vector<int>ans(n, 0);
+        // for(int i = 0 ; i < nums.size() ; i++)
+        // {
+        //     int prod = 1;
+        //     for(int j = 0 ; j < nums.size() ; j++)
+        //     {
+        //         if(j == i)
+        //             continue;
+        //         prod = prod * nums[j];
+        //     }
+        //     ans[i] = prod;
+        // }
+
+        // number 2 solution: have prod of the numbers to the right
+        vector<int>prodNums(n, 0);
+        int prod = 1;
+        for(int i = n - 1 ; i >=0 ; i--)
+        {
+            prodNums[i] = prod;
+            prod*= nums[i];
+        }
+        prod = 1;
         for(int i = 0 ; i < n ; i++)
         {
-            output[i] = curr;
-            curr*=nums[i];
+            ans[i] = prod * prodNums[i];
+            prod*= nums[i];
         }
-        curr = 1;
-        for(int i = n-1 ; i >=0 ; i--)
-        {
-            output[i]*= curr;
-            curr*=nums[i];
-        }
-        return output;
+        return ans;
     }
 };
