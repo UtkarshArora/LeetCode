83dd9c49cb8ffe6414597e6257aa0df1d64c2ebb
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-05-30 13:24:37 -0400

    Time: 0 ms (100.00%) | Memory: 21.7 MB (45.73%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -4,43 +4,29 @@ public:
         
 
         vector<vector<int>>result;
-        int insertIndex = -1;
+        int insertIndex = 0;
         int n = intervals.size();
         bool inserted = false;
-        // find where to insert Index
-        
-        //Case 1: intervals is empty
-        if(intervals.empty())
-        {
-            result.push_back(newInterval);
-            return result;
-        }
 
-        // Case 2: intervals[0] is greater than newInterval
-        if(!intervals.empty() && newInterval[1] < intervals[0][0])
+        // Case 1: intervals[0] is greater than newInterval
+        if(intervals.empty() || (!intervals.empty() && newInterval[1] < intervals[0][0]))
         {
             result.push_back(newInterval);
             inserted = true;
+            insertIndex = n;
         }
 
         // finding where to insert index
         for(int i = 0 ; i < n ; i++)
-        {
-            insertIndex++;
+        {   
             if(!inserted && newInterval[0] <= intervals[i][1])
             {
                 break;
             }
             result.push_back(intervals[i]);
+            insertIndex++;
         }
 
-        if(result.size() >= n)
-        {
-            if(!inserted){
-                result.push_back(newInterval);
-            }
-            return result;
-        }
         if(!inserted) {
             result.push_back(newInterval);
         }
