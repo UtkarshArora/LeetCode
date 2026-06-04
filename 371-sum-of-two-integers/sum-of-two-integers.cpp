480ae2e557ce45e33dd34f7735e027f0e822c9c2
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-04 16:37:45 -0400

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.78%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -2,32 +2,12 @@ class Solution {
 public:
     int getSum(int a, int b) {
 
-        vector<int>bitA(32,0);
-        vector<int>bitB(32,0);
-
-        for(int i = 31 ; i >= 0 && a!=0 ; i--)
+        while(b!=0)
         {
-            int bit = a % 2;
-            bitA[i] = bit;
-            a = a / 2;
+           int carry = a & b;
+           a = a ^ b;
+           b = (carry << 1);
         }
-        for(int i = 31; i >= 0 && b!=0 ; i--)
-        {
-            int bit = b%2;
-            bitB[i] = bit;
-            b = b / 2;
-        }
-        int carry = 0;
-        long long int mul = 1;
-        long long int finalRes = 0;
-        for(int i = 31 ; i >= 0 ; i--)
-        {
-            int sum = bitA[i] + bitB[i] + carry;
-            carry = sum/2;
-            int bit = sum % 2;
-            finalRes+=bit*mul;
-            mul*=2;
-        }
-        return finalRes;
+        return a;
     }
 };
