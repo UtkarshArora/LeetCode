973d01fd9febe3d7cbb9c1165ac5c6e36f2f6e32
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-12 20:00:16 -0400

    Time: 0 ms (100.00%) | Memory: 40.2 MB (59.46%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,52 +2,26 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        // number 1 solution: double loop
         int n = nums.size();
+        vector<int>op(n, 0);
+        // approach 1 : for each index, find prod of elem before i, find prod of elem after i, and store -> O(n^2) approach
 
-        vector<int>ans(n, 0);
-        // for(int i = 0 ; i < nums.size() ; i++)
-        // {
-        //     int prod = 1;
-        //     for(int j = 0 ; j < nums.size() ; j++)
-        //     {
-        //         if(j == i)
-        //             continue;
-        //         prod = prod * nums[j];
-        //     }
-        //     ans[i] = prod;
-        // }
+        // approach 2 : to optimize this, store the elements in an array, then just compute -> use prefix and suffix
 
-        // number 2 solution: have prod of the numbers to the right, basically we store the product so that we don't have to use 2 loops
-        // vector<int>prodNums(n, 0);
-        // int prod = 1;
-        // for(int i = n - 1 ; i >=0 ; i--)
-        // {
-        //     prodNums[i] = prod;
-        //     prod*= nums[i];
-        // }
-        // prod = 1;
-        // for(int i = 0 ; i < n ; i++)
-        // {
-        //     ans[i] = prod * prodNums[i];
-        //     prod*= nums[i];
-        // }
-
-        // number 3 solution: traverse from left, update in the final array
-        // traverse from right, update in the final array
+        // approach 3: no need to store prefix, compute prefix on the go, just store suffix, that will be our output array as well
 
         int prod = 1;
-        for(int i = 0 ; i < n ; i++)
+        for(int i = n-1; i >=0 ; i--)
         {
-            ans[i] = prod;
+            op[i] = prod;
             prod*=nums[i];
         }
         prod = 1;
-        for(int i = n-1 ; i >=0 ; i--)
+        for(int i = 0 ; i < n; i++)
         {
-            ans[i] = ans[i] * prod;
-            prod = prod * nums[i];
+            op[i] = op[i]*prod;
+            prod*=nums[i];
         }
-        return ans;
+        return op;
     }
 };
