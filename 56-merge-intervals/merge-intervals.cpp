fd608b8570428b24047c44b058170de3fc4b58a2
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-13 15:50:50 -0400

    Time: 4 ms (65.93%) | Memory: 23.8 MB (59.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-        vector<vector<int>>ans;
+        // sort it by start time, then easy to compare if the intervals are overlapping or not
+        
+        int n = intervals.size();
         sort(intervals.begin(), intervals.end());
+        vector<vector<int>>ans;
         ans.push_back(intervals[0]);
-        for(int i = 1; i < intervals.size() ; i++)
+        for(int i = 1 ; i < n ; i++)
         {
-            vector<int>v1 = intervals[i];
-            if(ans.back()[1] >= v1[0])
+            if(ans.back()[1] >= intervals[i][0])
             {
-                ans.back()[0] = min(ans.back()[0], v1[0]);
-                ans.back()[1] = max(ans.back()[1], v1[1]);
+                ans.back()[0] = min(ans.back()[0], intervals[i][0]);
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
             else
-                ans.push_back(v1);
+            {
+                ans.push_back(intervals[i]);
+            }
         }
         return ans;
     }
