bec0e8da27b3b51bdf17abd5c14eb6bc64372509
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-12 20:49:26 -0400

    Time: 43 ms (57.70%) | Memory: 46.7 MB (35.11%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,21 +5,25 @@ public:
         int n = nums.size();
         int total = 0;
         // creating all possible combinations of subarrays is O(n^2)
-        for(int i = 0 ; i < n; i++)
+        vector<int>prefix(n,0);
+        prefix[0] = nums[0];
+
+        for(int i = 1 ; i < n; i++)
         {
-            int sum = nums[i];
-            if(sum == k)
-            {
-                total+=1;
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        
+        unordered_map<int,int>mp;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(prefix[i] == k){
+                total++;
             }
-            for(int j = i+1; j < n ; j++)
+            if(mp.find(prefix[i]-k)!=mp.end())
             {
-                sum+=nums[j];
-                if(sum == k)
-                {
-                    total+=1;
-                }
+                total+=mp[prefix[i]-k];
             }
+            mp[prefix[i]]++;
         }
         return total;
     }
