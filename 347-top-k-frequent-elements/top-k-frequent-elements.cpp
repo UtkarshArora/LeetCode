2a575e6cfd27f39282eec5974b01f9b74d9ab07d
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-13 13:39:18 -0400

    Time: 7 ms (15.64%) | Memory: 17.9 MB (62.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -9,19 +9,26 @@ public:
         {
             freq[num]++;
         }
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
         for(auto p : freq)
         {
             pair<int,int>p1 = make_pair(p.second, p.first);
-            pq.push(p1);
+            if(pq.size() < k)
+            {
+                pq.push(p1);
+            }
+            else if(pq.top().first < p1.first)
+            {
+                pq.pop();
+                pq.push(p1);
+            }
         }
         int count = 0;
-        while(count < k)
+        while(!pq.empty())
         {
             pair<int,int>p = pq.top();
             pq.pop();
             ans.push_back(p.second);
-            count++;
         }
         return ans;
     }
