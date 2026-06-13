3d18eb24f2704453448e928de6a8abfc34db7deb
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-13 13:26:26 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (39.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
-
-        // store number its frequency in hashmap -> O(n)
-        unordered_map<int,int>map1;
-
+      
+        unordered_map<int,int>freq;
+        vector<int>ans;
         for(int num : nums)
         {
-            map1[num]++;        
+            freq[num]++;
         }
-
-        // store pair of freq and number in the max priority queue -> O(nlogn)
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        for(auto p1 : map1)
+        priority_queue<pair<int,int>>pq;
+        for(auto p : freq)
         {
-            pair<int,int>p = make_pair(p1.second, p1.first);
-            pq.push(p);
-            if(pq.size() > k)
-                pq.pop();
+            pair<int,int>p1 = make_pair(p.second, p.first);
+            pq.push(p1);
         }
-
-        // retrieve top k elements from the heap.   -> O(klogn)
-        // int count = 0;
-        vector<int>finalAns;                
-        while(!pq.empty())
+        int count = 0;
+        while(count < k)
         {
-            auto p1 = pq.top(); pq.pop();
-            finalAns.push_back(p1.second);
+            pair<int,int>p = pq.top();
+            pq.pop();
+            ans.push_back(p.second);
+            count++;
         }
-        return finalAns;
+        return ans;
     }
 };
