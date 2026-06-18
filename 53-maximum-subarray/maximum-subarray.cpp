0dc1bf74560670288652bd8c5af5f9e286e9551a
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-18 08:56:00 -0400

    Time: 16 ms (4.76%) | Memory: 71.8 MB (14.41%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
+
+    int maxSub(vector<int>& nums, int start, int end)
+    {
+        if(start > end)
+            return INT_MIN;
+        
+        if(start == end)
+            return nums[start];
+        
+        int mid = (end + start)/2;
+        int leftSum = 0, rightSum = 0;
+        for(int i = mid-1, currSum = 0 ; i>=start; i--)
+        {
+            currSum+=nums[i];
+            leftSum = max(currSum, leftSum);
+        }
+        for(int i = mid+1, currSum = 0 ; i <= end ; i++)
+        {
+            currSum+=nums[i];
+            rightSum = max(currSum, rightSum);
+        }
+        int left = maxSub(nums, start, mid-1);
+        int right = maxSub(nums, mid+1, end);
+        return max(leftSum + rightSum + nums[mid], max(left, right));
+    }
     int maxSubArray(vector<int>& nums) {
         
-        int currSum = nums[0];
-        int maxSum = nums[0];
-
-        for(int i = 1 ; i < nums.size() ; i++)
-        {
-            currSum = max(currSum + nums[i], nums[i]);
-            maxSum = max(currSum, maxSum);
-        }
-        return maxSum;
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        return maxSub(nums, left, right);
     }
 };
