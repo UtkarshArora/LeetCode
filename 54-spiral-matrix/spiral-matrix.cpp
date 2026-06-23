559e3a54406bceaafe70524d3d6869830d13fbee
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-23 15:42:15 -0400

    Time: 0 ms (100.00%) | Memory: 9.6 MB (14.76%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,85 +1,48 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        
-        int n_rows = matrix.size(), n_cols = matrix[0].size();
-        //vector<vector<bool>>visited(n, vector<bool>(m, false));
-        int count = n_rows * n_cols;
-        int total = 0;
-        int index = 0;
-        int diff = 0;
-        int ele_count = n_cols;
-        int num = 0;
-        vector<int>ans;
-        while(total < count && ele_count > 0)
-        {
-            while(num < ele_count && total<count)
-            {
-                int i = index / n_cols;
-                int j = index % n_cols;
-                ans.push_back(matrix[i][j]);
-                index++; num++;
-                total++;
+
+        int numRows = matrix.size();
+        int numCols = matrix[0].size();
+        int currRow = 0, currCol = 0;
+        vector<vector<bool>>visited(numRows, vector<bool>(numCols, false));
+        int count = numRows * numCols;
+        vector<int> res;
+        while (count > 0) {
+            while (currCol < numCols && !visited[currRow][currCol]) {
+                res.push_back(matrix[currRow][currCol]);
+                visited[currRow][currCol] = true;
+                count--;
+                currCol++;
             }
-            // if(total == count)
-            //     break;
-            diff++;
-            ele_count = n_rows - diff;
-            num = 0;
-            index--;
-            while(num < ele_count && total<count)
-            {
-                index = index + n_cols;
-                int i = index / n_cols;
-                int j = index % n_cols;
-                ans.push_back(matrix[i][j]);
-                num++;
-                total++;
+            currRow++;
+            currCol--;
+            while (currRow < numRows && !visited[currRow][currCol]) {
+                res.push_back(matrix[currRow][currCol]);
+                visited[currRow][currCol] = true;
+                count--;
+                currRow++;
             }
-            // if(total == count)
-            //     break;
-            num = 0;
-            ele_count = n_cols - diff;
-            while(num < ele_count && total<count)
-            {
-                index--;
-                int i = index / n_cols;
-                int j = index % n_cols;
-                ans.push_back(matrix[i][j]);
-                num++;
-                total++;
+            currCol--;
+            currRow--;
+            while (currCol >= 0 && !visited[currRow][currCol]) {
+                res.push_back(matrix[currRow][currCol]);
+                visited[currRow][currCol] = true;
+                count--;
+                currCol--;
             }
-            // if(total == ele_count)
-            //     break;
-            diff++;
-            num = 0;
-            ele_count = n_rows - diff;
-            while(num < ele_count && total<count)
-            {
-                index = index - n_cols;
-                int i = index / n_cols;
-                int j = index % n_cols;
-                ans.push_back(matrix[i][j]);
-                num++;
-                total++;
+            currCol++;
+            currRow--;
+            while (currRow >= 0 && !visited[currRow][currCol]) {
+                res.push_back(matrix[currRow][currCol]);
+                visited[currRow][currCol] = true;
+                count--;
+                currRow--;
             }
-            // if(total == ele_count)
-            //     break;
-            num = 0;
-            ele_count = n_cols - diff;
-            index++;
-            // cout<<"curent index:"<<index<<endl;
-            // cout<<"diff : "<<diff<<endl;
-            // cout<<"final ele_count" << ele_count;
-            // cout<<"size:"<<ans.size()<<", total:"<<total<<endl;
-            // for(int num : ans)
-            // {
-            //     cout<<num<<" ";
-            // }
-            // cout<<endl;
-            // cout<<"ele_count:"<<ele_count<<endl;
-            // cout<<"index:"<<index<<endl;
+            currRow++;
+            currCol++;
+            //cout<<"count "<<count<<endl;
         }
-        return ans;
+        return res;
     }
 };
