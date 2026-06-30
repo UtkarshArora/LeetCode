122309e7b314ac831a53da0ab91b3192eae59898
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-30 15:21:10 -0400

    Time: 2 ms (22.61%) | Memory: 9.2 MB (22.77%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
 
-    int numDecHelper(string s, int index, vector<int>&memo)
+    int numDecHelper(string s, int index)
     {
-        if(index == s.size())
-        {
+        if(index == s.size()){
             return 1;
         }
-        if(s[index] == '0')
-        {
+        if(s[index] == '0'){
             return 0;
         }
-        if(memo[index]!=0){
-            return memo[index];
-        }
-        int ans = numDecHelper(s, index+1, memo);
+        int ans = numDecHelper(s, index+1);
         if(index+1 < s.size())
         {
             int num = stoi(s.substr(index, 2));
-            if(num >= 10 && num <=26)
+
+            if(num >=10 && num <=26)
             {
-                ans+=numDecHelper(s, index+2, memo);
+                ans+=numDecHelper(s, index+2);
             }
         }
-        memo[index] = ans;
         return ans;
     }
     int numDecodings(string s) {
+        
+        // at each index i, I check if index-1 or substring with index-2...index-1 is valid
 
         int len = s.size();
-        vector<int>memo(len, 0);
-        return numDecHelper(s, 0, memo);
+        vector<int>dp(len+1);
+        dp[len] = 1;
+        for(int i = len - 1 ; i>=0 ; i--)
+        {
+            if(s[i] == '0')
+            {
+                dp[i] = 0;
+            }
+            else
+            {
+                dp[i] = dp[i+1];
+                if(i+1 < len)
+                {
+                    int num = stoi(s.substr(i, 2));
+                    if(num >=10 && num <=26)
+                    {
+                        dp[i]+=dp[i+2];
+                    }
+                }
+            }
+        }
+        return s.empty()? 0 : dp[0];
     }
 };
