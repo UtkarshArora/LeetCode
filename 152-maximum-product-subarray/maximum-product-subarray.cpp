c0ad5df16b56b98d9c2de66b6d4e1f7a182f0294
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-06-30 16:19:37 -0400

    Time: 359 ms (5.56%) | Memory: 17.9 MB (23.11%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // n^2 solution
 
         int maxProd = INT_MIN;
-        for(int i = 0 ; i < nums.size(); i++)
+        for(int i = 0 ; i < nums.size() ; i++)
         {
             int prod = nums[i];
             maxProd = max(prod, maxProd);
-            for(int j = i+1; j < nums.size() ; j++)
-            {   
+            for(int j = i+1 ; j < nums.size() ; j++)
+            {
                 prod*=nums[j];
                 maxProd = max(prod, maxProd);
             }
