7332c4dda728d1375596d519d6a5d3688d5ec389
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-07-13 04:06:40 -0400

    Time: 55 ms (52.04%) | Memory: 77.7 MB (39.69%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        
-        int n = points.size();
-        if(k == n)
+        priority_queue<pair<double,int>, vector<pair<double, int>>, greater<pair<double, int>>>pq;
+
+        vector<vector<int>>res;
+
+        for(int i = 0 ; i < points.size() ; i++)
         {
-            return points;
-        }
-        vector<vector<int>>ans;
-        priority_queue<pair<double,int>>pq;
-        for(int i = 0 ; i < n ; i++)
-        {
-            int x = points[i][0];
-            int y = points[i][1];
-            double dist = sqrt(pow(x, 2) + pow(y, 2));
-            pq.push({dist, i});
-            if(pq.size() > k){
-                pq.pop();
-            }
+            vector<int>v1 = points[i];
+            int x = v1[0], y = v1[1];
+            double val = sqrt(pow(x, 2) + pow(y, 2));
+            pq.push(make_pair(val, i));
         }
         int count = 0;
         while(count < k)
         {
-            pair<double,int>p1 = pq.top();
+            pair<double, int>p1 = pq.top();
             pq.pop();
-            int index = p1.second;
-            ans.push_back(points[index]);
             count++;
+            int index = p1.second;
+            res.push_back(points[index]);
         }
-        return ans;
+        return res;
     }
 };
