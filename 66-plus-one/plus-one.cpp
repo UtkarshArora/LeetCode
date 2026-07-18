1cabb1c1eb0499bf32550c6a4aaed86d7b1952d9
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-07-18 19:30:31 -0400

    Time: 0 ms (100.00%) | Memory: 11.4 MB (95.79%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
+        vector<int>ans;
+
         int n = digits.size();
-        if(digits[n-1]!=9)
+        int carry = 1;
+        for(int i = n-1 ; i >=0 ; i--)
         {
-            digits[n-1]++;
+            int sum = digits[i] + carry;
+            carry = sum >=10 ? 1 : 0; 
+            sum = sum % 10;
+            ans.push_back(sum);
         }
-        else
-        {
-            int m = n-1;
-            while(m>0 && digits[m]==9)
-            {
-                digits[m] = 0;
-                m--;
-            }
-            if(m == 0 && digits[m]==9)
-            {
-                digits[m]=1;
-                digits.push_back(0);
-            }
-            else
-                digits[m]++;
+        if(carry){
+            ans.push_back(1);
         }
-        return digits;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
