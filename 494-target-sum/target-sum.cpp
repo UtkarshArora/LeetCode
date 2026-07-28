dbfa7c9dbc7112cd101759fd95ff6dbb79a45633
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-07-27 21:51:21 -0400

    Time: 154 ms (32.24%) | Memory: 60.5 MB (7.61%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
 
-    int numWays(vector<int>& nums, int sum, int target, int index, vector<vector<int>>&dp, int maxSum)
+    int numWays(vector<int>& nums, int sum, int target, int index, vector<unordered_map<int,int>>&dp)
     {
         if(index == nums.size()){
-            if(sum - maxSum == target)
+            if(sum == target)
             {
                 return 1;
             }
             return 0;
         }
-        if(dp[index][sum]!=-1)
+        if(dp[index].find(sum)!=dp[index].end())
         {
             return dp[index][sum];
         }
-        int ans1 = numWays(nums, sum + nums[index], target, index+1, dp, maxSum);
-        int ans2 = numWays(nums, sum - nums[index], target, index+1, dp, maxSum);
+        int ans1 = numWays(nums, sum + nums[index], target, index+1, dp);
+        int ans2 = numWays(nums, sum - nums[index], target, index+1, dp);
         dp[index][sum] = ans1 + ans2;
         return ans1+ans2;
     }
@@ -25,12 +25,8 @@ public:
         if(n == 0){
             return 0;
         }
-        int maxSum = 0;
-        for(int num : nums){
-            maxSum+=num;
-        }
-        vector<vector<int>>dp(n, vector<int>(2*maxSum+2, -1));
-        int ans = numWays(nums, maxSum, target, 0, dp, maxSum);
+        vector<unordered_map<int,int>>dp(n);
+        int ans = numWays(nums, 0, target, 0, dp);
         return ans;
     }
 };
