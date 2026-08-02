3095a248af5c2f3904678c6c657bb0ef37040dca
UtkarshArora <49580321+UtkarshArora@users.noreply.github.com>
2026-08-01 22:38:31 -0400

    Time: 18 ms (77.66%) | Memory: 25.5 MB (54.76%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,20 +3,18 @@ class Solution:
         # 2 loops-> O(n^2) time complexity, O(1) space
         # for every element, store the product of elements on left and right
 
-        prefix = [1]*len(nums)
-        suffix = [1]*len(nums)
+        # suffix = [1]*len(nums)
         ans = [1]*len(nums)
-        prod = 1
-        for i in range(len(nums)):
-            prefix[i] = prod
-            prod*=nums[i]
+
         prod = 1
         for i in range(len(nums)-1, -1, -1):
-            suffix[i] = prod
+            ans[i] = prod
             prod*=nums[i]
         
+        prod = 1
         for i in range(len(nums)):
-            ans[i] = suffix[i]*prefix[i]
+            ans[i] = prod*ans[i]
+            prod = prod*nums[i]
 
         return ans
         
