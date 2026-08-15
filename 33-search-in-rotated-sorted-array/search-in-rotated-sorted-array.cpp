ab129a93c340141fad051d015f540f2bd07e6720
Utkarsh Arora <49580321+UtkarshArora@users.noreply.github.com>
2026-08-15 15:13:53 -0400

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.78%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        
-        int start = 0, end = nums.size() - 1;
 
-        while(start <= end)
+    int findMinimum(vector<int>& nums)
+    {
+        int start = 0, end = nums.size()-1;
+        while(start < end)
         {
-            //cout<<"start:"<<start<<", end:"<<end<<endl;
             int mid = start + (end - start)/2;
-            if(nums[mid] == target)
-                return mid;
-            
-            if(nums[start] > nums[mid])
-            {
-                if(target > nums[mid] && target <= nums[end])
-                    start = mid+1;
-                else
-                    end = mid-1;
+            if(nums[mid] < nums[end]){
+                end = mid;
             }
-            else
-            {
-                if(target < nums[mid] && target >= nums[start])
-                {
-                    end = mid - 1;
-                }
-                else
-                    start = mid + 1;
+            else{
+                start = mid+1;
             }
         }
-        return -1;
+        return start;
+    }
+
+    int binarySearch(vector<int>&nums, int start, int end, int target)
+    {
+        if(start < 0 || end < 0 || start >= nums.size() || end >=nums.size()){
+            return -1;
+        }
+        while(start < end)
+        {
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return nums[start] == target ? start : -1;
+    }
+    int search(vector<int>& nums, int target) {
+        
+        // time complexity -> O(n), space-> O(1)
+        // get inflection point or the minimum value from nums
+        // apply binary search in these ranges [start, index-1] [index, end]
+        //[7, 10, 0, 1, 2, 5, 6]
+        int index = findMinimum(nums);
+        cout<<index<<endl;
+        // if(nums[index] == target){
+        //     return index;
+        // }
+        int ans1 = binarySearch(nums, 0, index-1, target);
+        int ans2 = binarySearch(nums, index, nums.size()-1, target);
+        return ans1!=-1? ans1 : ans2;
     }
 };
