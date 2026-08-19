8b80480e40d37fbfd94c4c1e177d52422bee98aa
Utkarsh Arora <49580321+UtkarshArora@users.noreply.github.com>
2026-08-19 16:53:06 -0400

    Time: 90 ms (34.13%) | Memory: 45.5 MB (40.33%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
-int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
 
+        //[[2,1,1],[2,3,1],[3,4,1],[2,4,0]]
 
-        vector<int>timings(n+1, INT_MAX);
-        timings[k] = 0;
+        vector<vector<int>>adjMatrix(n + 1, vector<int>(n + 1, -1));
+
+        for (int i = 0; i < times.size(); i++) {
+            vector<int>v1 = times[i];
+            adjMatrix[v1[0]][v1[1]] = v1[2];
+        }
+
+        int totalEdges = n * (n - 1);
         int count = 0;
-        for(int i = 1 ; i < n ; i++)
-        {
-            count = 0;
-            bool changed = false;
-            for(vector<int>&v1 : times)
-            {
-                int x = v1[0], y = v1[1], w = v1[2];
-                if(timings[x]!=INT_MAX)
-                {
-                    if(timings[y] > timings[x] + w)
-                    {
-                        timings[y] = timings[x] + w;
-                        changed = true;
+        vector<int>dist(n + 1, INT_MAX);
+        dist[k] = 0;
+        priority_queue<int, vector<int>, greater<int>>pq;
+        pq.push(k);
+        while(!pq.empty()) {
+    
+            bool timeChanged = false;
+            int index = pq.top(); 
+            pq.pop();
+            
+            for (int i = 1; i <= n; i++) {
+                if (adjMatrix[index][i]!=-1) {
+                    if (dist[i] > dist[index] + adjMatrix[index][i]) {
+                        dist[i] = dist[index] + adjMatrix[index][i];
+                        timeChanged = true;
+                        pq.push(i);
                     }
                 }
             }
-            if(!changed)
-                break;
+            count++;
         }
-        int ans = *max_element(timings.begin()+1, timings.end());
-        return ans!=INT_MAX? ans : -1;
+        int maxVal = 0;
+        for(int i = 1; i <=n ; i++)
+        {
+            if(dist[i] == INT_MAX)
+            {
+                return -1;
+            }
+            maxVal = max(maxVal, dist[i]);
+        }
+        return maxVal;
     }
 };
